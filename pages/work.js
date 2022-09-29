a9c5d719e9488395ef6c4e2fb575238556c04fd0
Afaq Khan <afaqk3203@gmail.com>
2022-09-29 09:37:47 +0500

    Instagram Component is ready.

---
+++ pages/work.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+const work = () => {
+  return (
+    <>
+      <div>
+        <Hero
+          heading="My Work"
+          message="This is my recent work travelling the world"
+        />
+      </div>
+    </>
+  );
+};
+
+export default work;
